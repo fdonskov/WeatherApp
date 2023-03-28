205fc464538f1a818a64d0fa1ed514312bb99523
fdonskov <fedya_donskov@mail.ru>
2023-03-28 15:00:13 +0300

    Preliminary project setup

---
+++ WeatherApp/Sources/Application/SceneDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  SceneDelegate.swift
+//  WeatherApp
+//
+//  Created by Федор Донсков on 28.03.2023.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        self.window = UIWindow(windowScene: windowScene)
+        let navigationController = UINavigationController(rootViewController: ViewController())
+        window?.rootViewController = navigationController
+        window?.makeKeyAndVisible()
+    }
+}
+
