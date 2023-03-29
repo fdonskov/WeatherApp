bf566d12197311a627c9268429f08bd3f09574a5
fdonskov <fedya_donskov@mail.ru>
2023-03-29 06:48:40 +0300

    Added NetworkService and data model

---
+++ WeatherApp/Sources/Services/NetworkService.swift
@@ -0,0 +1,45 @@
+//
+//  NetworkService.swift
+//  WeatherApp
+//
+//  Created by Федор Донсков on 28.03.2023.
+//
+
+import Foundation
+
+class NetworkService {
+
+    static let shared = NetworkService()
+    
+    private let baseURLString = "https://api.weatherapi.com/v1"
+    private let apiKey = "9da93b4c507f441a88f230306232803"
+
+    func fetchWeatherData(city: String, completion: @escaping (Result<Data, Error>) -> Void) {
+
+        let urlString = "\(baseURLString)/current.json?key=\(apiKey)&q=\(city)&aqi=no"
+
+        guard let url = URL(string: urlString) else {
+            completion(.failure(NSError(domain: "", code: 0, userInfo: [NSLocalizedDescriptionKey: "Invalid URL"])))
+            return
+        }
+
+        let session = URLSession.shared
+
+        let task = session.dataTask(with: url) { data, response, error in
+
+            if let error = error {
+                completion(.failure(error))
+                return
+            }
+
+            guard let data = data else {
+                completion(.failure(NSError(domain: "", code: 0, userInfo: [NSLocalizedDescriptionKey: "No data received"])))
+                return
+            }
+            
+            completion(.success(data))
+        }
+
+        task.resume()
+    }
+}
