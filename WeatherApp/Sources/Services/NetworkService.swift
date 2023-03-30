909ecbc8756e76a4ecae3835dfc19644e9b401dd
fdonskov <fedya_donskov@mail.ru>
2023-03-30 07:09:34 +0300

    Updated NetworkService

--- WeatherApp/Sources/Services/NetworkService.swift
+++ WeatherApp/Sources/Services/NetworkService.swift
@@ -10,38 +10,42 @@ import Foundation
 class NetworkService {
     
     // https://api.weatherapi.com/v1/current.json?key=9da93b4c507f441a88f230306232803&q=Moscow&aqi=no
-
+    
     static let shared = NetworkService()
     
-    private let baseURLString = "https://api.weatherapi.com/v1"
-    private let apiKey = "9da93b4c507f441a88f230306232803"
-
+    private let baseURLString = Resources.NetworkService.baseURL
+    private let apiKey = Resources.NetworkService.apiKey
+    
     func fetchWeatherData(city: String, completion: @escaping (Result<Data, Error>) -> Void) {
-
+        
         let urlString = "\(baseURLString)/current.json?key=\(apiKey)&q=\(city)&aqi=no"
-
+        
         guard let url = URL(string: urlString) else {
-            completion(.failure(NSError(domain: "", code: 0, userInfo: [NSLocalizedDescriptionKey: "Invalid URL"])))
+            completion(.failure(NSError(domain: "",
+                                        code: 0,
+                                        userInfo: [NSLocalizedDescriptionKey: Resources.NetworkService.invalodUrl])))
             return
         }
-
+        
         let session = URLSession.shared
-
+        
         let task = session.dataTask(with: url) { data, response, error in
-
+            
             if let error = error {
                 completion(.failure(error))
                 return
             }
-
+            
             guard let data = data else {
-                completion(.failure(NSError(domain: "", code: 0, userInfo: [NSLocalizedDescriptionKey: "No data received"])))
+                completion(.failure(NSError(domain: "",
+                                            code: 0,
+                                            userInfo: [NSLocalizedDescriptionKey: Resources.NetworkService.noData])))
                 return
             }
             
             completion(.success(data))
         }
-
+        
         task.resume()
     }
 }
